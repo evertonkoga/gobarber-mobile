8d3d4ee0e0737d5c521fb06cf961177327feea56
Everton Koga <everton.koga@sotran.com.br>
2021-02-15 08:06:15 -0300

    commit inicial

---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => {
+  return (
+    <Container />
+  )
+};
+
+export default CreateAppointment;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Profile: React.FC = () => {
+  return (
+    <Container />
+  )
+};
+
+export default Profile;
