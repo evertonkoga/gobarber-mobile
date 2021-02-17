1f3f6979f9af9506143488be3ce69826a68c7fda
Everton Koga <everton.koga@sotran.com.br>
2021-02-17 20:27:33 -0300

    implementado tela dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,18 +2,24 @@ import React, { createContext, useCallback, useContext, useState, useEffect } fr
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
-interface AuthState {
-  token: string;
-  user: object;
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
 }
 
+interface AuthState {
+  token: string;
+  user: User;
+}
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -33,6 +39,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -54,6 +62,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
